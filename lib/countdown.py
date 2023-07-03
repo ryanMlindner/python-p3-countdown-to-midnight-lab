5eaa52b71277f7a284eb993709e6781353189ab6
Ryan Lindner <ryan.m.lindner@gmail.com>
2023-07-03 15:45:30 -0600

    lab completed

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,15 @@
 # your code goes here!
+import time
+
+def countdown(index):
+  while index > 0:
+    print(f'{index} SECOND(S)!')
+    index -= 1
+  print('HAPPY NEW YEAR!')
+
+def countdown_with_sleep(index):
+  while index > 0:
+    print(f'{index} SECOND(S)!')
+    time.sleep(1)
+    index -= 1
+  print('HAPPY NEW YEAR!')
